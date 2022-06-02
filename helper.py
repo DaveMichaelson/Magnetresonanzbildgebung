bcd70054fb2fdf28cf41c4acfcc96864fb9dccec
David Seehaus <davidseehaus@mac.com>
2022-06-03 00:54:49 +0200

    Versuch Notebook 3

--- helper.py
+++ helper.py
@@ -7,14 +7,20 @@ def visualize_kspaces(kspaces):
     fig, axs = plt.subplots(n, 2, squeeze=False)
 
     for i in range(n):
-        axs[i, 1].imshow(np.abs(np.fft.ifftshift(np.fft.ifft2(kspaces[:, :, i]))))
+        axs[i, 1].imshow(np.angle(kspaces[:, :, i]))
         axs[i, 0].imshow(np.abs(kspaces[:, :, i]), norm=colors.LogNorm())
+
+def visualize_single_kspace(kspace):
+    fig, axs = plt.subplots(1, 2)
+    axs[1].imshow(np.angle(kspace))
+    axs[0].imshow(np.abs(kspace), norm=colors.LogNorm())
+
 
 def visualize_seperate_kspaces(kspaces):
     n = kspaces.shape[2]
 
     for i in range(n):
         fig, axs = plt.subplots(1, 2, squeeze=False)
-        axs[0, 1].imshow(np.abs(np.fft.ifftshift(np.fft.ifft2(kspaces[:, :, i]))))
+        axs[0, 1].imshow(np.angle(kspaces[:, :, i]))
         axs[0, 0].imshow(np.abs(kspaces[:, :, i]), norm=colors.LogNorm())
 

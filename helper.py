ff380804f4f27373dc04709cf48d4db0229c7582
David Seehaus <davidseehaus@mac.com>
2022-06-03 13:59:38 +0200

    Starting task 4

--- helper.py
+++ helper.py
@@ -9,11 +9,13 @@ def visualize_kspaces(kspaces):
     for i in range(n):
         axs[i, 1].imshow(np.angle(kspaces[:, :, i]))
         axs[i, 0].imshow(np.abs(kspaces[:, :, i]), norm=colors.LogNorm())
+    plt.show()
 
 def visualize_single_kspace(kspace):
     fig, axs = plt.subplots(1, 2)
     axs[1].imshow(np.angle(kspace))
     axs[0].imshow(np.abs(kspace), norm=colors.LogNorm())
+    plt.show()
 
 
 def visualize_seperate_kspaces(kspaces):
@@ -23,4 +25,26 @@ def visualize_seperate_kspaces(kspaces):
         fig, axs = plt.subplots(1, 2, squeeze=False)
         axs[0, 1].imshow(np.angle(kspaces[:, :, i]))
         axs[0, 0].imshow(np.abs(kspaces[:, :, i]), norm=colors.LogNorm())
+    plt.show()
 
+def zero_outside_circle(array, r):
+    ret_array = np.copy(array)
+    x = np.arange(array.shape[0])
+    y = np.arange(array.shape[1])
+    cx = array.shape[0] / 2
+    cy = array.shape[1] / 2
+
+    ret_array[(x[np.newaxis, :] - cx) ** 2 + (y[:, np.newaxis] - cy) ** 2 >= r ** 2] = 0
+    return ret_array
+
+def ifft(ksapce, axes=None):
+    return np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(ksapce, axes=axes), axes=axes), axes=axes)
+
+def fft(image, axes=None):
+    return np.fft.fftshift(np.fft.fft2(np.fft.fftshift(image, axes=axes), axes=axes), axes=axes)
+
+def visualize_images(images):
+    fig, axs = plt.subplots(1, images.shape[2])
+    for i in range(images.shape[2]):
+        axs[i].imshow(np.abs(images[:, :, i]))
+    plt.show()
